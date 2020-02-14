6beaa8f5210e42ed56eb6b4e7d9c3ba4fcbec7fb
kreuzerk <kevin.kreuzer90@icloud.com>
2020-02-14 21:40:14 +0100

    chore(filtertable): use harness tests

--- src/app/filter-table/filter-table.component-new-style.spec.ts
+++ src/app/filter-table/filter-table.component-new-style.spec.ts
@@ -7,10 +7,16 @@ import {MatTableModule} from '@angular/material/table';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {By} from '@angular/platform-browser';
 import {DebugElement} from '@angular/core';
+import {HarnessLoader} from '@angular/cdk/testing';
+import {TestbedHarnessEnvironment} from '@angular/cdk/testing/testbed';
+import {MatTableHarness} from '@angular/material/table/testing';
+import {MatTab} from '@angular/material/tabs';
+import {MatInputHarness} from '@angular/material/input/testing';
 
-describe('FilterTableComponent Old', () => {
+describe('FilterTableComponent', () => {
   let component: FilterTableComponent;
   let fixture: ComponentFixture<FilterTableComponent>;
+  let loader: HarnessLoader;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -28,31 +34,28 @@ describe('FilterTableComponent Old', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(FilterTableComponent);
     component = fixture.componentInstance;
+    loader = TestbedHarnessEnvironment.loader(fixture);
     fixture.detectChanges();
   });
 
   it('should have the correct table headings', async () => {
     const expectedHeadings = ['Firstname', 'Name', 'Title', 'Alive', 'Home', 'Culture'];
-    const headings = fixture.debugElement.queryAll(By.css('.mat-table thead tr th'));
-    const headingTexts = headings.map((headingElement: DebugElement) => headingElement.nativeElement.innerText);
-    expect(headingTexts).toEqual(expectedHeadings);
+    const table = await loader.getHarness<MatTableHarness>(MatTableHarness);
+    const headerRows = await table.getHeaderRows();
+    expect(await headerRows[0].getCellTextByIndex()).toEqual(expectedHeadings);
   });
 
-  it('should initially display three rows in the table', () => {
-    const rows = fixture.debugElement.queryAll(By.css('.mat-table tbody tr'));
+  it('should initially display three rows in the table', async () => {
+    const table = await loader.getHarness<MatTableHarness>(MatTableHarness);
+    const rows = await table.getRows();
     expect(rows.length).toBe(3);
   });
 
-  it('should filter the table when we enter startk as a filter text', done => {
-    const inputField = fixture.debugElement.query(By.css('.mat-input-element'));
-    inputField.nativeElement.value = 'Stark';
-    const keyupEvent = new Event('keyup');
-    inputField.nativeElement.dispatchEvent(keyupEvent);
-    fixture.detectChanges();
-    fixture.whenStable().then(() => {
-      const rows = fixture.debugElement.queryAll(By.css('.mat-table tbody tr'));
-      expect(rows.length).toBe(1);
-      done();
-    });
+  it('should filter the table when we enter startk as a filter text', async () => {
+    const inputField = await loader.getHarness<MatInputHarness>(MatInputHarness);
+    const table = await loader.getHarness<MatTableHarness>(MatTableHarness);
+    await inputField.setValue('Stark');
+    const rows = await table.getRows();
+    expect(rows.length).toBe(1);
   });
 });
